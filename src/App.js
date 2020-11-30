07b22cc621ec80b2683afa7cf7be1cdf2c66b3fa
It-001 <58259550+It-001@users.noreply.github.com>
2020-11-30 22:14:37 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./styles.css";
 
+const luckyNumber = "7";
+
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!!</h2>
+      <h1>Hello Arfin</h1>
+      <ul>
+        <li>React</li>
+        <li>Node</li>
+        <li>Jquery</li>
+      </ul>
+      <p>Your lucky number: {Math.floor(Math.random() * 10)}</p>
     </div>
   );
 }
